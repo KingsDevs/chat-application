77c716f03fd9c2a462b53d4144b90838545f5702
KingsDevs <sunnyckair@gmail.com>
2021-08-25 02:02:09 +0800

    added login page

--- application/modules/auth/views/login_page.php
+++ application/modules/auth/views/login_page.php
@@ -1,5 +1,31 @@
 <div class="container">
-    <div class="text-center">
-        <h3>Chat Application Login</h3>
-    </div>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title text-center">Chat Application Login</h3>
+                        <br>
+                        <form action="#" method="POST">
+                            <div class="mb-3">
+                                <label class="form-label">Username</label>
+                                <input type="text" class="form-control" placeholder="username" name="username">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Password</label>
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                            <br>
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-success">Login</button>
+                            </div>
+                            <br>
+                            <p class="text-center">You don't have an account? <a href="#">Sign Up</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    
 </div>
