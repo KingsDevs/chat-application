f9737fea9adb80022df1fd1f31153bc38cf1805e
KingsDevs <sunnyckair@gmail.com>
2021-08-25 02:17:30 +0800

    database added

--- application/config/database.php
+++ application/config/database.php
@@ -74,9 +74,9 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'chat_application',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
 	'database' => '',
 	'dbdriver' => 'mysqli',
