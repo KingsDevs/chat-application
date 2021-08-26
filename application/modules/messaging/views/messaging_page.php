069a626a2af95ecf7cfc90dd0df2df856f0b1c4f
KingsDevs <sunnyckair@gmail.com>
2021-08-27 02:27:54 +0800

    added modal to add contact

--- application/modules/messaging/views/messaging_page.php
+++ application/modules/messaging/views/messaging_page.php
@@ -16,7 +16,7 @@
                         <div style="color:white;"><?php echo $this->session->userdata('userdata')['firstname'].' '.$this->session->userdata('userdata')['lastname'] ?></div>
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="<?php echo site_url('logout'); ?>">Logout</a></li>
+                        <li><a class="dropdown-item" href="<?php echo site_url('logout'); ?>" style="font-size: smaller;">Logout</a></li>
                     </ul>
                 </div>
 				<i class="fa fa-chevron-down expand-button" aria-hidden="true"></i>
@@ -67,7 +67,7 @@
 			</ul>
 		</div>
 		<div id="bottom-bar">
-			<button id="addcontact"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> <span>Add contact</span></button>
+			<button id="addcontact" data-bs-toggle="modal" data-bs-target="#addContactModal"><i class="fa fa-user-plus fa-fw" aria-hidden="true"></i> <span>Add contact</span></button>
 			<button id="settings"><i class="fa fa-cog fa-fw" aria-hidden="true"></i> <span>Settings</span></button>
 		</div>
 	</div>
@@ -125,4 +125,25 @@
 			</div>
 		</div>
 	</div>
+</div>
+
+
+<!-- modals -->
+
+<div class="modal fade" id="addContactModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
