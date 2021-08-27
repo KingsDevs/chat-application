d85b576940729b4cf5a79f2f6dc4d06e78490c81
KingsDevs <sunnyckair@gmail.com>
2021-08-28 02:39:59 +0800

    ajax whyyy

--- application/modules/messaging/views/scripts/scripts.php
+++ application/modules/messaging/views/scripts/scripts.php
@@ -1,4 +1,5 @@
-<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script><script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script>
 
 $(document).ready(function(){
@@ -8,22 +9,25 @@ $(document).ready(function(){
         e.preventDefault();
         var search = $('#searchUserBox').val();
 
-        // $.ajax(
-            
-        // );
+        if(search != '')
+        {
+            // $.ajax({
+            // url:<?php echo base_url(); ?>'search_user',
+            // type: "post",
+            // dataType: "json",
+            // data:{
+            //     search : search
+            // },
+            // success : function(data)
+            // {
+            //     console.log(data);
+            // }
+        // });
+        alert(search);
+        }
+       
 
-        $.ajax({
-            url:<?php echo base_url(); ?>'search_user',
-            type: "post",
-            dataType: "json",
-            data:{
-                search : search
-            },
-            success : function(data)
-            {
-                console.log(data);
-            }
-        });
+        
     });
 });
 
